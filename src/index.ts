a3c0874fcbfaf442e12e20915b5a4eb0e599aa9f
Бабак Артем <babak@kinoplan.ru>
2019-10-14 17:34:49 +0300

    next

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-var body = document.querySelector('body');
 
-body.style.backgroundColor = '#000000';
-console.log(body);
+type Message = {
+  /* 3301c037-2223-499c-95b3-75d5761dde3f */
+  client_msg_id: string,
+  /* T02A9K56P */
+  team: string,
+  text: string,
+  /* 1570708949.005200 */
+  ts: string,
+  type: 'message' | any,
+  /* UL7413NR1 - my username */
+  user: string,
+}
 
+type Conversation = {
+  channel_actions_count: number,
+  channel_actions_ts: null,
+  has_more: boolean,
+  is_limited: boolean,
+  messages: Message[],
+  ok: boolean,
+  pin_count: number,
+}
+
+
+function logURL(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {urls: ["<all_urls>"]}
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,24 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true
+                    }
+                },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
 };
