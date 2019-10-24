4a83ed3c6779104ac86a464528af4440366180da
Бабак Артем <babak@kinoplan.ru>
2019-10-24 18:19:16 +0300

    next

--- app/routes/note_routes.ts
+++ app/routes/note_routes.ts
@@ -1,7 +1,45 @@
 import { Express } from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectID } from "mongodb";
 
 export default function (app: Express, dbClient: MongoClient) {
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    dbClient.db('notes').collection('notes').deleteOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + req.params.id + ' deleted!');
+      }
+    });
+  });
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    dbClient.db('notes').collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+
+    dbClient.db('notes').collection('notes').updateOne(details, note,(err, result) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(note);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
 
